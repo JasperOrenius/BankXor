ecfb9f812fe17781a4db4638cfde2981a4ca2e87
Jasper-Orenius <167324971+Jasper-Orenius@users.noreply.github.com>
2024-09-05 16:07:37 +0300

    Added accounts, withdraw and deposit functions, cleaned up code

--- BankXor/Properties/AssemblyInfo.cs
+++ BankXor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FileEncryptionUsingXOR")]
+[assembly: AssemblyTitle("BankXor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FileEncryptionUsingXOR")]
+[assembly: AssemblyProduct("BankXor")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
